f846456daf8f717e42b52ee61d1fb35339f9dee5
Gabriel <shalitmontagne.g@gmail.com>
2021-10-10 13:21:32 -0400

    commit tree cf1a78c97d3ca0752fa692f800a9ea6f92c98313

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function superbowlWin(record){
+    let found = record.find(object => object.result === "W")
+    if (found == null){
+        return found
+    }
+    else {
+        return found.year
+    }
+    
+    
+}
